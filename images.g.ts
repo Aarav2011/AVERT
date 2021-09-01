4c71ec2ab5a6a6a2b4a03564ba4d41a5e279ff54
Coder369 <89343196+Aarav2011@users.noreply.github.com>
2021-09-01 16:26:52 +0530

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,55 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "UkCG0guP@Gx/":
+            case "Apple":return img`
+. . . . . . . e c 7 . . . . . 
+. . . . e e e c 7 7 e e . . . 
+. . c e e e e c 7 e 2 2 e e . 
+. c e e e e e c 6 e e 2 2 2 e 
+. c e e e 2 e c c 2 4 5 4 2 e 
+c e e e 2 2 2 2 2 2 4 5 5 2 2 
+c e e 2 2 2 2 2 2 2 2 4 4 2 2 
+c e e 2 2 2 2 2 2 2 2 2 2 2 2 
+c e e 2 2 2 2 2 2 2 2 2 2 2 2 
+c e e 2 2 2 2 2 2 2 2 2 2 2 2 
+c e e 2 2 2 2 2 2 2 2 2 2 4 2 
+. e e e 2 2 2 2 2 2 2 2 2 4 e 
+. 2 e e 2 2 2 2 2 2 2 2 4 2 e 
+. . 2 e e 2 2 2 2 2 4 4 2 e . 
+. . . 2 2 e e 4 4 4 2 e e . . 
+`;
+            case "d5derdZ+JZqSnZl`B3gp":
+            case "Pastries":return img`
+. . . . . . . . . . b b b . . . 
+. . . . . . . . b e e 3 3 b . . 
+. . . . . . b b e 3 2 e 3 a . . 
+. . . . b b 3 3 e 2 2 e 3 3 a . 
+. . b b 3 3 3 3 3 e e 3 3 3 a . 
+b b 3 3 3 3 3 3 3 3 3 3 3 3 3 a 
+b 3 3 3 d d d d 3 3 3 3 3 d d a 
+b b b b b b b 3 d d d d d d 3 a 
+b d 5 5 5 5 d b b b a a a a a a 
+b 3 d d 5 5 5 5 5 5 5 d d d d a 
+b 3 3 3 3 3 3 d 5 5 5 d d d d a 
+b 3 d 5 5 5 3 3 3 3 3 3 b b b a 
+b b b 3 d 5 5 5 5 5 5 5 d d b a 
+. . . b b b 3 d 5 5 5 5 d d 3 a 
+. . . . . . b b b b 3 d d d b a 
+. . . . . . . . . . b b b a a . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
